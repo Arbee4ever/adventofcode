687605193bfc5f1c37bd13386bc4a05f1d7d560b
Arbee4ever <arbee@arbeeco.de>
2023-12-02 10:00:42 +0100

    - implement day 2

--- 2023/2/2.py
+++ 2023/2/2.py
@@ -5,33 +5,28 @@ arr = string.split(os.linesep)
 
 out = 0
 for line in arr:
-	redMax = 12
-	greenMax = 13
-	blueMax = 14
+	redCubes = 0
+	greenCubes = 0
+	blueCubes = 0
 
 	inputData = line.split("Game ")[1]
 	gameID = int(inputData.split(": ")[0])
 	gameData = inputData.split(": ")[1]
-	valid = True
 	for data in gameData.split("; "):
 		for cubes in data.split(", "):
 			cubesData = cubes.split(" ")
 			match cubesData[1]:
 				case "red":
-					if int(cubesData[0]) > redMax:
-						valid = False
-						break
+					if redCubes < int(cubesData[0]):
+						redCubes = int(cubesData[0])
 				case "green":
-					if int(cubesData[0]) > greenMax:
-						valid = False
-						break
+					if greenCubes < int(cubesData[0]):
+						greenCubes = int(cubesData[0])
 				case "blue":
-					if int(cubesData[0]) > blueMax:
-						valid = False
-						break
+					if blueCubes < int(cubesData[0]):
+						blueCubes = int(cubesData[0])
 				case _:
 					print("Error")
 					break
-	if valid:
-		out += gameID
+	out += (redCubes * greenCubes * blueCubes)
 print(out)
