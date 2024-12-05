30b9121e690d421006b95120ba76faf3a589951e
Arbee4ever <arbee@arbeeco.de>
2024-12-05 11:10:01 +0100

    - implement day 1/2 2024

--- 2024/1/2.py
+++ 2024/1/2.py
@@ -13,13 +13,18 @@ for x in arr:
 		leftX = int(x.split("   ")[0])
 		rightX = int(x.split("   ")[1])
 		left.append(leftX)
+		if not leftX in leftCount:
+			leftCount[leftX] = 0
 		leftCount[leftX] += 1
 		right.append(rightX)
+		if not rightX in rightCount:
+			rightCount[rightX] = 0
 		rightCount[rightX] += 1
 
 left.sort()
 right.sort()
 for y in range(len(left)):
-	if left[y].
+	if left[y] in rightCount:
+		out += int(left[y] * rightCount[left[y]])
 		
 print("Result: " + str(out))
