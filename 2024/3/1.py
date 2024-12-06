64da5262ff1ee374663b7c4695c4519f5468062d
Arbee4ever <arbee@arbeeco.de>
2024-12-06 14:31:33 +0100

    - reformat day 3 part 1 2024

--- 2024/3/1.py
+++ 2024/3/1.py
@@ -1,4 +1,3 @@
-import os
 import re
 
 string = open("input.txt", "r").read()
@@ -7,9 +6,9 @@ out = 0
 
 print(len(arr))
 for el in arr:
-    split = el.split(",")
-    x = int(split[0].split("(")[1])
-    y = int(split[1].split(")")[0])
-    out += x * y
-    
- print("Result: " + str(out))
+	split = el.split(",")
+	x = int(split[0].split("(")[1])
+	y = int(split[1].split(")")[0])
+	out += x * y
+
+print("Result: " + str(out))
