da6bf7d054078e394151b7731f732433ef364d21
Arbee4ever <arbee@arbeeco.de>
2024-12-14 16:14:35 +0100

    - reformat day 11 part 1 2024

--- 2024/11/1.py
+++ 2024/11/1.py
@@ -1,17 +1,17 @@
-import os
-
 string = open("input.txt", "r").read()
 arr = [int(x) for x in string.split(" ")]
 out = 0
+
 
 def blink(stone):
 	if stone == 0:
 		return [1]
 	elif len(str(stone)) % 2 == 0:
-		stoneLen = int(len(str(stone))/2)
+		stoneLen = int(len(str(stone)) / 2)
 		return [int(str(stone)[:stoneLen]), int(str(stone)[stoneLen:])]
 	else:
 		return [stone * 2024]
+
 
 result = [arr]
 print("result", str(result[0]))
